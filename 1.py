17bb19d7d35528ae7bd9140254275e93b70e15cf
Anshika Pandey <tanumansheer@gmail.com>
2026-08-25 23:34:38 +0530

    Add program to print numbers and calculate sum
    This program prints numbers from 1 to 10, identifies them as even or odd, and calculates their total sum.

---
+++ 1.py
@@ -0,0 +1,17 @@
+# Program to print numbers from 1 to 10
+# and calculate their sum and even/odd numbers
+
+sum = 0
+
+for i in range(1, 11):
+    print("Number:", i)
+
+    if i % 2 == 0:
+        print("Even number")
+    else:
+        print("Odd number")
+
+    sum = sum + i
+
+print("----------------")
+print("Total Sum =", sum)
